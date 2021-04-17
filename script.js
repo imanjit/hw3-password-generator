5596b9646a40a57c14323be12d84613a1640b135
Iman Bains <imanjitbains@yahoo.com>
2021-04-17 15:45:01 -0700

    Wrote password function with conditional statements and loops. Added prompts for user to choose how they would like to structure the password

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 var lowercase = "abcdefghijklmnopqrstuvwxyz".split("")
 var numbers = "0123456789".split("")
-var symbols = "!@#$%^&*()_-=+{[]}\\|:;'\"<>,.?/".split("")
+var specialChars = "!@#$%^&*()_-=+{[]}\\|:;'\"<>,.?/".split("")
 
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
+    var characters = [];
+    var result = "";
+    var length = prompt("How many characters would you like your password to be?");
+    if (length < 8 || length > 128) {
+        alert("Please choose a password between 8 and 128 characters.");
+    }
+    else {
+        if (confirm("Include uppercase?")) {
+            Array.prototype.push.apply(characters, uppercase);
+        }
+        if (confirm("Include lowercase?")) {
+            Array.prototype.push.apply(characters, lowercase);
+        }
+        if (confirm("Include numbers?")) {
+            Array.prototype.push.apply(characters, numbers);
+        }
+        if (confirm("Include special characters?")) {
+            Array.prototype.push.apply(characters, specialChars);
+        }
+        else {
+            for(var i = 0; i < length; i++) {
+                var random = math.floor(math.random() * characters.length);
+                result += characters[random];
+            }
+        }
+        document.getElementById("password").textContent = result;
+    }
+};
 generateBtn.addEventListener("click", writePassword);
